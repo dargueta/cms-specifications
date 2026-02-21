e6e488161dd3aa8cb51ce29c09679cb80e385477
Diego Argueta <dargueta@users.noreply.github.com>
2026-02-20 21:50:01 -0800

    (BROKEN) Kinda fix dependencies

--- code/schemas/dependencies.mk
+++ code/schemas/dependencies.mk
@@ -2,16 +2,16 @@ GENERIC_HEADER_DETAIL_TRAILER_FILE_COMPONENTS=records/detail.yaml records/header
 GENERIC_HEADER_DETAIL_TRAILER_VERSIONS=18.6 18.7 18.8
 GENERIC_HEADER_DETAIL_TRAILER_TARGETS=$(foreach f,$(GENERIC_HEADER_DETAIL_TRAILER_FILE_COMPONENTS),$(foreach v,$(GENERIC_HEADER_DETAIL_TRAILER_VERSIONS),$(v)/$(f)))
 
-BEQ4RX_RENDERED_YAML_TARGETS=$(addprefix file_formats/beq4rx,$(GENERIC_HEADER_DETAIL_TRAILER_TARGETS))
-BQN4_RENDERED_YAML_TARGETS=$(addprefix file_formats/bqn4,$(GENERIC_HEADER_DETAIL_TRAILER_TARGETS))
+BEQ4RX_RENDERED_YAML_TARGETS=$(addprefix build/beq4rx/,$(GENERIC_HEADER_DETAIL_TRAILER_TARGETS))
+BQN4_RENDERED_YAML_TARGETS=$(addprefix build/bqn4/,$(GENERIC_HEADER_DETAIL_TRAILER_TARGETS))
 ALL_RENDERED_YAML_TARGETS=$(BEQ4RX_RENDERED_YAML_TARGETS) $(BQN4_RENDERED_YAML_TARGETS)
 
 .DELETE_ON_FAILURE:
 
 # BEQ4RX -----------------------------------------------------------------------
-build/beq4rx/18.6/%: file_formats/beq4rx/18.7/%
-build/beq4rx/18.8/%: file_formats/beq4rx/18.7/%
+build/beq4rx/18.6/%: build/beq4rx/18.7/%
+build/beq4rx/18.8/%: build/beq4rx/18.7/%
 
 # BQN4 -------------------------------------------------------------------------
-build/bqn4/18.6/%: file_formats/bqn4/18.7/%
-build/bqn4/18.8/%: file_formats/bqn4/18.7/%
+build/bqn4/18.6/%: build/bqn4/18.7/%
+build/bqn4/18.8/%: build/bqn4/18.7/%
