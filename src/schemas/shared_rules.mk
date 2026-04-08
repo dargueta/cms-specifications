b5bec61b93c66d92712e666336ccee972ffc6188
Diego Argueta <dargueta@users.noreply.github.com>
2026-04-07 22:06:08 -0700

    Simplify Mermaid generation by using common rules

--- src/schemas/shared_rules.mk
+++ src/schemas/shared_rules.mk
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+.SECONDARY: $(CURDIR)/record_layout.mmd
+
 %-min.json: %.json
 	python3 -m scripts.minijson $< $@
 
@@ -10,6 +12,13 @@ $(TARGET_FORMAT_DIR)/%.json: $(CURDIR)/%.yaml
 	mkdir -p $(@D)
 	python3 -m scripts.postprocess_yaml --json -I $(FORMATS_ROOT)/_common -o $@ $<
 	check-jsonschema --schemafile $(TABLESCHEMA_URL) $@
+
+$(TARGET_FORMAT_DIR)/%/record_layout.mmd: $(CURDIR)/record_layout.mmd
+	mkdir -p $(@D)
+	ln -f $< $@
+
+$(CURDIR)/record_layout.mmd: $(CURDIR)/README.md
+	python3 -m scripts.mermaid_from_markdown $< $@
 
 $(TARGET_FORMAT_DIR)/%: $(CURDIR)/%
 	mkdir -p $(@D)
